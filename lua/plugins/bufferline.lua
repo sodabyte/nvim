c2e17cc15bad34ed13199a04bb0e8fd0f159a623
sodabyte <181641501+sodabyte@users.noreply.github.com>
2024-10-03 06:36:54 +0800

    Fix indenting

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,34 +9,31 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
 
     config = function()
-    require("bufferline").setup {
-      options = {
-        indicator = {
-          icon = " "
+      require("bufferline").setup {
+        options = {
+          indicator = {
+            icon = " "
+          },
+          separator_style = {
+            "", ""
+          },
+          always_show_bufferline = true,
+          sort_by = "insert_at_end",
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              highlight = "Directory",
+              separator = false -- Set to "true" to enable the def
+            }
+          },
         },
-        separator_style = {
-          "", ""
-        },
-        always_show_bufferline = true,
-        sort_by = "insert_at_end",
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            highlight = "Directory",
-            separator = false -- Set to "true" to enable the def
+        highlights = {
+          buffer_selected = {
+            italic = false, bold = false
           }
         },
-      },
-      highlights = {
-        buffer_selected = {
-          italic = false, bold = false
-        },
-        fill = {
-          bg = "#1E1E2E",
-        },
-      },
-    }
+      }
     end
   }
 }
